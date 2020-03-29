29aa59fea82555f655c79ee9dcdd514a6092b099
Dylan Troop <dylantroop@gmail.com>
2020-03-29 16:19:51 +0100

    Add biased asymptotic variance function and gradient same as in paper.

--- asymp_var.py
+++ asymp_var.py
@@ -1,11 +1,4 @@
 import numpy as np
-from scipy.stats import genpareto
-
-def avar_mle2(xi, sig):
-    var_sh = (1+xi)**2
-    var_sc = 2*sig**2 * (1+xi)
-    cov = -sig*(1+xi)
-    return np.asarray([[var_sh, cov], [cov, var_sc]])
 
 def avar_mle(xi, sig):
     var_sh = (1+xi)**2
@@ -14,23 +7,29 @@ def avar_mle(xi, sig):
     return np.asarray([[var_sh, cov], [cov, var_sc]])
 
 def Dsh(xi, sig, Fu, alph):
-    b = (1-Fu)/(1-alph)
+    b = (1-alph)/(1-Fu)
     x1 = 1/xi
     x2 = b**xi * np.log(b) / (1-xi)
     x3 = b**xi * (1-2*xi) / xi / (1-xi)**2
-    return sig/xi * (x1 + x2 - x3)
+    return sig/xi**2 - sig*b**(-xi)*(1-2*xi+xi*(1-xi)*np.log(b)) \
+            /xi**2/(1-xi)**2
 
 def Dsc(xi, sig, Fu, alph):
-    b = (1-Fu)/(1-alph)
-    return (b**xi + xi - 1) / xi / (1-xi)
+    b = (1-alph)/(1-Fu)
+    return (b**(-xi) + xi - 1) / xi / (1-xi)
 
 def asymp_var(xi, sig, Fu, alph):
     grad = np.asarray([Dsh(xi, sig, Fu, alph), Dsc(xi, sig, Fu, alph)])
     avmle = avar_mle(xi, sig)
     return np.matmul(np.matmul(grad, avmle), grad)
 
+def asymp_var_biased(xi, sig, xi_b, sig_b, Fu, alph):
+    grad = np.asarray([Dsh(xi_b, sig_b, Fu, alph), Dsc(xi_b, sig_b, Fu, alph)])
+    avmle = avar_mle(xi, sig)
+    return np.matmul(np.matmul(grad, avmle), grad)
+
 def bias(x, true):
-    return np.mean(x, axis=0) - true
+    return np.nanmean(x, axis=0) - true
 
 def mse(x, true):
-    return np.mean((x-true)**2, axis=0)
+    return np.nanmean((x-true)**2, axis=0)
